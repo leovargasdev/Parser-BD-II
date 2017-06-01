f48d406a4ba57d9280ce802d8acd716168816bce
LeeonardoVargas <leu1607@hotmail.com>
2017-06-01 00:05:16 -0300

    sepanado oque teve commit ou esta no check point

--- parser.java
+++ parser.java
@@ -1,17 +1,41 @@
+/*
+    * REDO --> transações comitadas (valor novo)
+    * UNDO --> sem CKPT e commit (valor antigo)
+*/
+
 import java.util.*;
 import java.io.*;
 class parser{
     public static void main(String [] args) throws IOException{
         BufferedReader ler = new BufferedReader(new FileReader(new File("log.txt")));//carrega o arquivo
         ArrayList<transacoes> transacoes = new ArrayList(); // ArrayList com os objetos da classe transacoes.java
-        String l = ler.readLine(), start = "start", commit = "Commit";
+        ArrayList<String> linhas = new ArrayList(), listaCheckPoint = new ArrayList(); // ArrayList com os objetos da classe transacoes.java
+        Boolean ckptFlag = false;
+        String l = ler.readLine(), start = "START", commit = "COMMIT", checkPoint = "CKPT";
         while(l != null){
-            l = l.replaceAll("[< >]", "");// remove os caracteres especiais e espaços da linha
+            l = l.replaceAll("[< >()]", "").toUpperCase();// remove os caracteres especiais e espaços da linha
+            linhas.add(l);
             if(l.startsWith(start)){// entra aqui quando eh uma nova transaçao, verificando se começa com "start"
                 String linhaT = l.replaceAll(start, "");// remove a palavra "start" da linha
-                transacoes.add(new transacoes(linhaT));// cria uma nova transação e add essa nova transação no ArrayList de transações
-            } else if(l.startsWith(commit)){// entra aqui quando eh um commit, verificando se começa com "commit"
-                //System.out.println("Commit: " + l);
+                if(linhaT.startsWith(checkPoint)){
+                    String[] ckpt = linhaT.replaceAll(checkPoint, "").split(",");
+                    for (int a = 0; a < ckpt.length; a++)
+                        for(int w = 0; w < transacoes.size(); w++ )
+                            if(ckpt[a].equals(transacoes.get(w).transacao))
+                                transacoes.get(w).setCkpt(); // seta a flag de check point da transação
+                    ckptFlag = true;
+                    //System.out.println("inicio ckpt: " + l);
+                } else {
+                    transacoes.add(new transacoes(linhaT));// cria uma nova transação e add essa nova transação no ArrayList de transações
+                }
+            } else if(l.startsWith(commit) && !ckptFlag){// entra aqui quando eh um commit, verificando se começa com "commit"
+                l = l.replaceAll(commit, "");
+                for(int w = 0; w < transacoes.size(); w++ )// percorre todas as transacoes já iniciada
+                    if(l.startsWith(transacoes.get(w).transacao))
+                        transacoes.get(w).commitTransacao();
+            } else if(l.contains(checkPoint)){
+                ckptFlag = false;
+                //System.out.println("fim ckpt: " + l);
             } else {
                 for(int w = 0; w < transacoes.size(); w++ )// percorre todas as transacoes já iniciada
                     if(l.startsWith(transacoes.get(w).transacao))// verifica qual transacao pertence essa operação
@@ -22,8 +46,12 @@ class parser{
         ler.close();
         System.out.println("transacoes: ");
         for(int i = 0; i < transacoes.size(); i++){
-            System.out.println(transacoes.get(i).transacao + ": ");
-            transacoes.get(i).getTransacoes();
+            if(!transacoes.get(i).commit || transacoes.get(i).ckpt){
+                System.out.println(transacoes.get(i).transacao + ": ");
+                transacoes.get(i).getTransacoes();
+            }
         }
+        /*for(int i = 0; i < linhas.size(); i++)
+            System.out.println(linhas.get(i)+ ": ");*/
     }
 }
--- transacoes.java
+++ transacoes.java
@@ -3,9 +3,13 @@ import java.io.*;
 class transacoes{
     public ArrayList<variavel> op = new ArrayList();// variavel das operações referente a transação
     public String transacao;
+    public Boolean commit; // flag para saber se foi dado um commit antes de um checkPoint ou depois
+    public Boolean ckpt; // flag para saber se uma transação esta no parametro do checkPoint
     //metodo construtor
     public transacoes(String t){
         this.transacao = t;
+        this.commit = false;
+        this.ckpt =  false;
     }
     public void insertT(String l){
         variavel v = new variavel(l.split(","));
@@ -14,5 +18,11 @@ class transacoes{
     public void getTransacoes(){
         for(int g = 0; g < this.op.size(); g++)
             this.op.get(g).getVariavel();
+    }
+    public void commitTransacao(){
+        this.commit = true;
+    }
+    public void setCkpt(){
+        this.ckpt = true;
     }
 }
